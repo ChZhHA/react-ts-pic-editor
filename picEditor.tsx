789cb28d17c074f8b1d90afe3cd390a06aa84760
ChZhHA <chzh314@hotmail.com>
2020-12-21 12:05:44 +0800

    1 binaries uploaded

--- picEditor.tsx
+++ picEditor.tsx
@@ -1,6 +1,23 @@
-import React from 'react';
-export default class PicEditor extends React.Component{
-  render(){
-    return null
+import React from "react";
+interface PicEditorProps {
+  src: string;
+}
+export default class PicEditor extends React.Component<PicEditorProps> {
+  private _canvas = document.createElement("canvas");
+  private _ctx: CanvasRenderingContext2D;
+  constructor(props: PicEditorProps) {
+    super(props);
+    this._ctx = this._canvas.getContext("2d") as CanvasRenderingContext2D;
   }
+  render() {
+    return (
+      <div>
+        <img src={this.props.src} onLoad={this.picLoaded} alt=""/>
+      </div>
+    );
+  }
+  picLoaded = e => {
+    const img = e.target as HTMLImageElement;
+    
+  };
 }
